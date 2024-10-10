8c8734c45a41990070eeebc4bfdca5a7cc8bb7ba
Saint-Patic <alexis.demarcq@hotmail.com>
2024-10-10 12:44:20 +0200

    remove commentary

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-// #include <sys/ipc.h>
-// #include <sys/shm.h>
-// #include <sys/sem.h>
-// #include <unistd.h>
-// #include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <unistd.h>
+#include <time.h>
 
-// float generate_random_time(float min_time, float max_time) {
-//   return min_time + ((float)rand() / RAND_MAX) * (max_time - min_time);
-// }
+float generate_random_time(float min_time, float max_time) {
+  return min_time + ((float)rand() / RAND_MAX) * (max_time - min_time);
+}
