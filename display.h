b4520206b66613d7aa9bc8cd8869834b800dd56f
Nathan <153100527+Akanaifu@users.noreply.github.com>
2024-11-21 11:59:07 +0100

    Delete display.h

--- display.h
+++
@@ -1,8 +0,0 @@
-#ifndef DISPLAY_H
-#define DISPLAY_H
-
-void display_practice_results(car_t cars[], int num_cars);
-void display_overall_best_times(car_t cars[], int num_cars);
-
-
-#endif //DISPLAY_H
